2bdcc15667b4da64b23106cddb52510163c9f66b
Mizer <contact@shambhalafm.fr>
2018-11-17 15:01:58 +0100

    [FIX] name of file & commentary

--- extracturl.sh
+++ extracturl.sh
@@ -8,10 +8,13 @@
 						#       Verison : 0.1.0    #
 						############################
 
-# this script allows to extract all the urls contained in the file index.html or a file of your choice
-# ce script permet d'extraire toutes les urls contenue dans le fichier index.html ou un fichier de votre choix
+# this script allows to extract all the urls contained in the file "sources.html" 
+# or a file of your choice and to write them in the file "readdable.txt"
+
+# ce script permet d'extraire toutes les urls contenue dans le fichier sources.html 
+# ou un fichier de votre choix et de les ecrirent dans le fichier "readdable.txt"
 
 
 
 # we grep all url
-grep -Eo 'https?://[^"]+' index.html  >> readable.txt
+grep -Eo 'https?://[^"]+' sources.html  >> readdable.txt
