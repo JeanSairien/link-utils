abefc987381ba8f93225c3c6d138e10b96ea8b93
Mizer <contact@shambhalafm.fr>
2018-11-17 17:51:09 +0100

    [FIX] try extract title

--- extracturl.sh
+++ extracturl.sh
@@ -15,10 +15,74 @@
 # ou un fichier de votre choix et de les ecrirent dans le fichier "readdable.txt"
 
 
-#not work 
+
+sed -n 's/^\ *"title": \"\(.*\)",/\1/p' indent.json >> readdable3.txt 
+echo \n | grep -Po '"'"uri"'"\s*:\s*"\K([^"]*)' indent.json >> readdable3.txt
+
+# sed -n '/"title"/,/^\],$/s/^*"uri": "\(.*\)",/\1/p' indent.json >> readdable3.txt
+
+
+
+
+
+
+
+
+
+
+
+#------grep-------------
+# we grep all url (OK)
+# grep -Eo 'https?://[^"]+' indent.json  >> readdable2.txt
+#grep -Po '"'"uri"'"\s*:\s*"\K([^"]*)' $1
+
+#(NOT WORK)
 #grep -o '<H3>.*</H3>' bookmarks.html >> readdable2.txt
+
+
+
+#-------sed-----------
+# sed -n 's/^\ *"title": \"\(.*\)",/\1/p' indent.json >> readdable3.txt (OK)
+
+#(NOT WORK) 
+#sed -n 's/^\ *"uri": \"\(.*\)",/\1/p' indent.json
+
+#EXEMPLE
 #sed -n  '/<div class=\"contents\">/,/<\/div>/p' fichier.html  bookmarks.html >> readdable2.txt 
 
-# we grep all url
 
-#& grep -Eo 'https?://[^"]+' bookmarks.html  >> readdable2.txt
+
+#----------------vendors--------------------
+
+
+# title(){
+# 	command	sed -n 's/^\ *"title": \"\(.*\)",/\1/p' indent.json
+# 	return TITRE
+# }
+
+# # echo $TITRE
+
+# uri(){
+# 	command grep -Po '"'"uri"'"\s*:\s*"\K([^"]*)' indent.json
+# 	return URI
+# }	
+
+# for url in URI{
+	
+# }
+
+# done
+# echo "\n" $url
+
+# title
+
+# uri
+
+# echo -e "\n" $TITLE >> readdable3.txt | echo -e "\n" $URI >> readdable3.txt
+
+
+
+
+
+
+#echo  -e "\n"   >> readdable3.txt | echo  -e "\n"   >> readdable3.txt 
